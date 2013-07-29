f2c261f05e67a0647fac48c609875e55796c773a
Adrienne Walker <enne@quisquo.us>
2013-07-28 23:22:54 -0700

    Create Meteorite package manifest

---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  summary: "Fries Android prototyping framework, repackaged for Meteor"
+});
+
+Package.on_use(function (api, where) {
+  api.add_files('lib/fonts/icomoon.svg', 'client');
+  api.add_files('lib/fonts/icomoon.ttf', 'client');
+  api.add_files('lib/fonts/icomoon.woff', 'client');
+  api.add_files('lib/fonts/icomoon.eot', 'client');
+  api.add_files('lib/css/base.css', 'client');
+  api.add_files('lib/css/stack.css', 'client');
+  api.add_files('lib/css/typography.css', 'client');
+  api.add_files('lib/css/content.css', 'client');
+  api.add_files('lib/css/spinners.css', 'client');
+  api.add_files('lib/css/sliders.css', 'client');
+  api.add_files('lib/css/chevrons.css', 'client');
+  api.add_files('lib/css/tabs.css', 'client');
+  api.add_files('lib/css/icomoon.css', 'client');
+  api.add_files('lib/css/lists.css', 'client');
+  api.add_files('lib/css/action-bars.css', 'client');
+  api.add_files('lib/css/forms.css', 'client');
+  api.add_files('lib/css/buttons.css', 'client');
+  api.add_files('lib/js/spinners.js', 'client');
+  api.add_files('lib/js/stack.js', 'client');
+  api.add_files('lib/js/tabs.js', 'client');
+  api.add_files('lib/js/action-bars.js', 'client');
+});
